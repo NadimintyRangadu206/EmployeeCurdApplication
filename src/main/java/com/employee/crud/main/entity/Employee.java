9fa0067c47698e586c0efb5c480302e6dbf91412
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2023-11-22 22:55:13 +0530

    Adding required fields for entity class and adding database properties

--- src/main/java/com/employee/crud/main/entity/Employee.java
+++ src/main/java/com/employee/crud/main/entity/Employee.java
@@ -1,8 +1,100 @@
 package com.employee.crud.main.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Table(name="txn_Employee")
+@Table(name = "txn_Employee")
+@Entity
 public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	@Column(name = "Employee_Id")
+	private int EmployeeId;
+
+	@Column(name = "Employee_Name")
+	private String EmployeeName;
+
+	@Column(name = "address")
+	private String address;
+
+	@Column(name = "age")
+	private int age;
+
+	@Column(name = "Company_Name")
+	private String companyName;
+
+	@Column(name = "dept")
+	private String dept;
+
+	@Column(name = "salary")
+	private double salary;
+
+	public int getEmployeeId() {
+		return EmployeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		EmployeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return EmployeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		EmployeeName = employeeName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [EmployeeId=" + EmployeeId + ", EmployeeName=" + EmployeeName + ", address=" + address
+				+ ", age=" + age + ", companyName=" + companyName + ", dept=" + dept + ", salary=" + salary + "]";
+	}
 
 }
