9d215ac7a43b5c5f4c49673be49ad8823d50a380
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2023-11-23 10:26:28 +0530

    Creating EmployeeRequest Class

--- src/main/java/com/employee/crud/main/controller/EmployeeController.java
+++ src/main/java/com/employee/crud/main/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.employee.crud.main.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+	
+	public ResponseEntity<String> saveEmployee(){
+		
+		return null;
+	}
 }
--- src/main/java/com/employee/crud/main/entity/Employee.java
+++ src/main/java/com/employee/crud/main/entity/Employee.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Employee {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 	@Column(name = "Employee_Id")
 	private int EmployeeId;
